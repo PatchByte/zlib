1a0913dfdedce4574f207d2e15bebaa124280097
Hendrik A <lightjavacode@gmail.com>
2023-04-08 20:52:05 +0200

    removed build dir

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/zlib-fork
-# Build directory: E:/zlib-fork/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
-set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "E:/zlib-fork/CMakeLists.txt;222;add_test;E:/zlib-fork/CMakeLists.txt;0;")
